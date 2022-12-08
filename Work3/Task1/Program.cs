3d154908d0841b0cc84f853b564d66139d7249a7
Sherya <djvoolf@yandex.ru>
2022-12-09 02:48:06 +0400

    Work3.Task1

---
+++ Work3/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        string N = Console.ReadLine();
+        int lenght = N.Length;
+
+        int first = 0;
+        int last = lenght - 1;
+
+        if (N[first] == N[last])
+        {
+            first++;
+            last--;
+            Console.WriteLine(N + " -> Полиндром");
+        }
+        else
+        {
+            Console.WriteLine(N + " -> не Полиндром");
+        }
+
+
+    }
+}
