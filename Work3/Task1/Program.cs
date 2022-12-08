02a55152f50de356e429e2ba14cbac6c1060879d
Sherya <djvoolf@yandex.ru>
2022-12-09 02:36:12 +0400

    Work3.Task1

---
+++ Work3/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 1
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Введите число");
+        string N = Console.ReadLine();
+        int lenght = N.Length;
+
+        int first = 0;
+        int last = lenght - 1;
+
+        if (N[first] == N[last])
+        {
+            first++;
+            last--;
+            Console.WriteLine(N + " -> Полиндром");
+        }
+        else 
+        {
+            Console.WriteLine(N + " -> не Полиндром");
+        }
+
+
+    }
+
+}
