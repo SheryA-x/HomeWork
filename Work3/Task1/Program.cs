56a005394c4f18f3b21410ce9adffa11d6118241
Sherya <djvoolf@yandex.ru>
2022-12-09 19:32:24 +0400

    Task1

--- Work3/Task1/Program.cs
+++ Work3/Task1/Program.cs
@@ -1,21 +1,30 @@
-﻿internal class Program
+﻿/*
+Задача 1
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.Write("Введите число: ");
         int number = Int32.Parse(Console.ReadLine());
-
         int revers_res = revers(number);
 
         if (number == revers_res)
         {
-            Console.WriteLine("Полиндром");
+            Console.WriteLine("Палиндром");
         }
         else
         {
-            Console.WriteLine("не Полиндром");
+            Console.WriteLine("не Палиндром");
         }
-            static int revers(int x)
+
+        //////////////////////////
+            int revers(int x) // метод
         {
             int result = 0;
             int number = x;
