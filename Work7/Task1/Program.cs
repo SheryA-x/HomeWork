f41ef28d1aed897ffcc4d229c441efba74396407
Sherya <djvoolf@yandex.ru>
2022-12-21 02:32:29 +0400

    Task1, Task2.

---
+++ Work7/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 1.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. 
+Тип данных для элементов выбрать double, наличие ненулевой дробной части обязательно.
+
+m = 3, n = 4.
+0.5 7.96 -2.78 -0.2
+1.7 -3.3 8.574 -9.9
+8.5 7.87 -7.1 9.15
+*/
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задайте количество строк массива");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Задайте количество столбцов массива");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Random rnd = new Random();
+
+        Console.WriteLine($"m = {m}, n = {n}.");
+        double[,] arr = new double[m,n];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                arr[i,j] = Math.Round( (rnd.NextDouble()*100/10), 3);
+
+                Console.Write($"{arr[i, j]} ");
+
+                if (j == n -1)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Work7/Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Задача 2. Напишите программу, которая на вход принимает позиции (два индекса) элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+[1, 7]->такого числа в массиве нет
+*/
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задайте количество строк массива");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Задайте количество столбцов массива");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Укажите два индекса массива");
+        int ind1 = Convert.ToInt32(Console.ReadLine());
+        int ind2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        int[,] arr = new int[m, n];
+        Random rnd = new Random();
+
+        int numberRequested = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                arr[i, j] = rnd.Next(1, 10);
+                Console.Write($"{arr[i, j]} ");
+
+                if (j == n - 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        if (n <= ind1 & m <= ind2)
+        {
+            Console.WriteLine($"[{ind1},{ind2}] -> такого числа в массиве нет");
+        }
+        else
+        {
+            numberRequested = arr[ind1, ind2];
+            Console.WriteLine($"число в массиве [{ind1},{ind2}] -> {numberRequested}");
+        }
+    }
+}
